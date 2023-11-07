a1cde05c9da99e9f9959ce74e2de3faaa00bb9bc
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-11-07 20:52:30 +0300

    refactoring function

--- main.js
+++ main.js
@@ -1,28 +1,38 @@
 const circle = document.getElementById("circle");
 const resBtn = document.getElementById("reset");
 const stopBtn = document.getElementById("stop");
+
 let count = 0;
 let active = false;
+let idInterval;
 
-const animation = () => {
+const circleAnimate = () => {
   count++;
 
-  idInterval = requestAnimationFrame(animation);
+  idInterval = requestAnimationFrame(circleAnimate);
 
-  if (count <= 500) {
-    circle.style.width = count + "px";
-    circle.style.height = count + "px";
+  if (count < 500) {
+    circle.style.width = 100 + count + "px";
+    circle.style.height = 100 + count + "px";
   } else {
     cancelAnimationFrame(idInterval);
   }
 };
 
-resBtn.addEventListener("click", () => {
+stopBtn.addEventListener("click", () => {
   if (active) {
-    cancelAnimationFrame(idInterval);
+    idInterval = requestAnimationFrame(circleAnimate);
+    active = false;
   } else {
-    idInterval = requestAnimationFrame(animation);
+    cancelAnimationFrame(idInterval);
+    active = true;
   }
 });
 
-animation();
+resBtn.addEventListener("click", () => {
+  circle.style.width = 100 + "px";
+  circle.style.height = 100 + "px";
+  count = 0;
+});
+
+circleAnimate();
